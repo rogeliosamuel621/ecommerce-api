ca54e25b7e845b0f54e2bef89f18e7d7aabe2cc1
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-10-25 04:58:11 -0600

    swagger docs added

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { appConfig } from './common/config/env';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /**
+   * Swagger configuration
+   */
+  const config = new DocumentBuilder()
+    .setTitle('E-commerce API')
+    .setDescription('API for E-commerce actions')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  /**
+   * Start of the app
+   */
   await app.listen(appConfig().PORT);
 }
 bootstrap();
