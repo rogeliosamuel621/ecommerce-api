42c35f0001fe3f026033941aa6bc57babc83e73f
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-11-06 15:40:12 -0700

    removed unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './app/auth/auth.module';
 import { ConfigModule } from './common/config/config.module';
 import { DatabaseModule } from './common/database/database.module';
 
 @Module({
   imports: [DatabaseModule, ConfigModule, AuthModule],
-  providers: [AppService],
-  controllers: [AppController],
 })
 export class AppModule {}
