0c05a0d8f5ca820598f8206a36d3bdf24b94d0fb
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-11-06 15:41:07 -0700

    app.module.ts file moved

--- src/app.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { AuthModule } from './app/auth/auth.module';
-import { ConfigModule } from './common/config/config.module';
-import { DatabaseModule } from './common/database/database.module';
-
-@Module({
-  imports: [DatabaseModule, ConfigModule, AuthModule],
-})
-export class AppModule {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '../common/config/config.module';
+import { DatabaseModule } from '../common/database/database.module';
+
+@Module({
+  imports: [DatabaseModule, ConfigModule, AuthModule],
+})
+export class AppModule {}
