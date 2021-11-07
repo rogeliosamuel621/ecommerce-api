01d71875e71078b46690cb00d35950c6d3d5175d
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-11-07 00:02:34 -0700

    ProductRepository created

--- src/app/product/repository/product.repository.ts
+++ src/app/product/repository/product.repository.ts
@@ -1,6 +1,16 @@
 import { Injectable } from '@nestjs/common';
+import { ICategory } from '../interfaces/categories.interface';
+import { IProduct } from '../interfaces/product.interface';
+import { content } from './content';
 
+const { categories, products } = content;
 @Injectable()
 export class ProductRepository {
-  getAllProducts() {}
+  getAllProducts(): IProduct[] {
+    return products;
+  }
+
+  getAllCategories(): ICategory[] {
+    return categories;
+  }
 }
