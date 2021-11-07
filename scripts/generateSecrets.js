58db6cbde96314db55fa0e5c4c33af84d33d1fe4
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-11-06 23:30:27 -0700

    script for generate secret keys added

---
+++ scripts/generateSecrets.js
@@ -0,0 +1,9 @@
+const crypto = require('crypto');
+
+const apiKey = crypto.randomBytes(64).toString('hex');
+const jwtSecret = crypto.randomBytes(64).toString('hex');
+
+console.log('copy the output below and paste it in your env.local \n');
+
+console.log(`API_KEY=${apiKey}`);
+console.log(`JWT_SECRET=${jwtSecret}`);
--- src/common/config/schemas/schemas.config.ts
+++ src/common/config/schemas/schemas.config.ts
@@ -2,5 +2,7 @@ import * as Joi from 'joi';
 
 export const schemaValidations = Joi.object({
   PORT: Joi.number().required(),
+  API_KEY: Joi.string().required(),
+  JWT_SECRET: Joi.string().required(),
   MONGO_DB_URI: Joi.string().required(),
 });
