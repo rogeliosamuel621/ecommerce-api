099516fcc475b1baccce859586618be2c5835c2e
Rogelio Samuel Moreno Corrales <rogeliosamuel621@gmail.com>
2021-11-06 23:00:30 -0700

    http status code changed

--- src/app/user/controllers/authUser.controller.ts
+++ src/app/user/controllers/authUser.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { LoginDto } from '../dto/login.dto';
 import { RegisterDto } from '../dto/register.dto';
 import { AuthUserService } from '../services/authUser.service';
@@ -13,6 +13,7 @@ export class AuthUserController {
   }
 
   @Post('login')
+  @HttpCode(200)
   async login(@Body() body: LoginDto) {
     return await this.authUserService.login(body);
   }
